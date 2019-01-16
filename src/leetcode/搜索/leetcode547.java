ea33c1485cd167b77a70fa3270fc38187b82eb5d
李锴 <1121176110@qq.com>
2019-01-16 12:51:53 +0800

    [add] 添加547题

---
+++ src/leetcode/搜索/leetcode547.java
@@ -0,0 +1,41 @@
+package leetcode.搜索;
+
+/**
+ * 班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
+
+ 给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，否则为不知道。你必须输出所有学生中的已知的朋友圈总数。
+ */
+public class leetcode547 {
+
+    public static void main(String[] args){
+        int[][] M = new int[][]{{1,1,0},{1,1,0},{0,0,1}};
+        System.out.println(findCircleNum(M));
+
+    }
+
+    private static int n;
+
+    public static int findCircleNum(int[][] M) {
+
+        n = M.length;
+        int circleNum = 0;
+        boolean[] hasVisited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!hasVisited[i]) {
+                dfs(M, i, hasVisited);
+                circleNum++;
+            }
+        }
+        return circleNum;
+    }
+
+    private static void dfs(int[][] M, int i, boolean[] hasVisited) {
+
+        hasVisited[i] = true;
+        for (int k = 0; k < n; k++) {
+            if (M[i][k] == 1 && !hasVisited[k]) {
+                dfs(M, k, hasVisited);
+            }
+        }
+    }
+}
