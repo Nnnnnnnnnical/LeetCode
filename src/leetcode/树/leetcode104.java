4c47bad6b926336e67bae696ac2abf86df08fe36
李锴 <1121176110@qq.com>
2019-01-26 12:33:48 +0800

    [add] 添加110题

--- src/leetcode/树/leetcode104.java
+++ src/leetcode/树/leetcode104.java
@@ -8,5 +8,13 @@ public class leetcode104 {
         }
 
         return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+        /*
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        if(Math.abs(l-r)>1){
+            result = false;
+        }
+        return 1+Math.max(l,r);
+         */
     }
 }
