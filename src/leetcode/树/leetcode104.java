99579ad2f2910fbd0ef97dc33646ebbfbec23017
李锴 <1121176110@qq.com>
2019-01-27 12:47:06 +0800

    [add] 添加104题注解

--- src/leetcode/树/leetcode104.java
+++ src/leetcode/树/leetcode104.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 给定一个二叉树，找出其最大深度。
+
+ 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ */
 public class leetcode104 {
 
     public static void main(String[] args) throws IOException {
