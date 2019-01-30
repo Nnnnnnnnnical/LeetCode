e02b3b785b8a4a513094168951109d3cdc880173
李锴 <1121176110@qq.com>
2019-01-30 13:35:10 +0800

    [revise] 添加104题

--- src/leetcode/树/leetcode104.java
+++ src/leetcode/树/leetcode104.java
@@ -29,11 +29,12 @@ public class leetcode104 {
 
         return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
         /*
+        if(root == null){
+            return 0;
+        }
+
         int l = maxDepth(root.left);
         int r = maxDepth(root.right);
-        if(Math.abs(l-r)>1){
-            result = false;
-        }
         return 1+Math.max(l,r);
          */
     }
