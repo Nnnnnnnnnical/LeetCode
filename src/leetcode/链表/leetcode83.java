ddf3e8584f5271c3cffbf54011864a6a7da003d5
李锴 <1121176110@qq.com>
2019-01-30 14:44:24 +0800

    [revise] 添加83题解法

--- src/leetcode/链表/leetcode83.java
+++ src/leetcode/链表/leetcode83.java
@@ -1,10 +1,26 @@
 package leetcode.链表;
 
+/**
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ */
 public class leetcode83 {
 
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) return head;
-        head.next = deleteDuplicates(head.next);
-        return head.val == head.next.val ? head.next : head;
+        //解法1
+//        if (head == null || head.next == null) return head;
+//        head.next = deleteDuplicates(head.next);
+//        return head.val == head.next.val ? head.next : head;
+
+        //解法2
+        ListNode p = head;
+        while (p != null) {
+            ListNode q = p.next;
+            while (q != null && q.val == p.val) {
+                q = q.next;
+            }
+            p.next = q;
+            p = p.next;
+        }
+        return head;
     }
 }
