c3251c55d609e1ba5e924fb04781e069d99a412c
李锴 <1121176110@qq.com>
2019-01-30 14:22:00 +0800

    [revise] 添加21题解法

--- src/leetcode/链表/leetcode21.java
+++ src/leetcode/链表/leetcode21.java
@@ -7,20 +7,44 @@ import org.w3c.dom.NodeList;
  */
 public class leetcode21 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null){
-            return l2;
-        }
-        if(l2 == null){
-            return l1;
+
+        ListNode temp = new ListNode(-1);
+        ListNode result = temp;
+
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                temp.next = l1;
+                l1 = l1.next;
+                temp = temp.next;
+            }else {
+                temp.next = l2;
+                l2 = l2.next;
+                temp = temp.next;
+            }
         }
 
-        if(l1.val < l2.val){
-            l1.next = mergeTwoLists(l1.next,l2);
-            return l1;
+        if(l1!=null){
+            temp.next = l1;
+        }else {
+            temp.next = l2;
         }
-        else {
-            l2.next = mergeTwoLists(l1,l2.next);
-            return l2;
-        }
+
+        return result.next;
+
+//        if(l1 == null){
+//            return l2;
+//        }
+//        if(l2 == null){
+//            return l1;
+//        }
+//
+//        if(l1.val < l2.val){
+//            l1.next = mergeTwoLists(l1.next,l2);
+//            return l1;
+//        }
+//        else {
+//            l2.next = mergeTwoLists(l1,l2.next);
+//            return l2;
+//        }
     }
 }
