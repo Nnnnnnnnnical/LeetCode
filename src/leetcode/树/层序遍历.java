dd1ebc33529b98a659c6faa01a7752666febb117
李锴 <1121176110@qq.com>
2019-02-23 14:01:34 +0800

    [revise] 修改层序遍历实现

--- src/leetcode/树/层序遍历.java
+++ src/leetcode/树/层序遍历.java
@@ -7,15 +7,44 @@ import java.util.Queue;
 
 public class 层序遍历 {
 
-    public List<Integer> levelTraversal(TreeNode root){
-        List<Integer> ans = new ArrayList<>();
-        Queue<TreeNode> treeNode = new LinkedList<>();
-        treeNode.add(root);
-        while (!treeNode.isEmpty()){
-            TreeNode node = treeNode.poll();
-            ans.add(node.val);
-            treeNode.add(node.left);
-            treeNode.add(node.right);
+    //    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+//        ArrayList<Integer> ans = new ArrayList<>();
+//        Queue<TreeNode> queue = new LinkedList<>();
+//        if(root == null){
+//            return ans;
+//        }
+//        queue.add(root);
+//        while (!queue.isEmpty()){
+//            TreeNode temp = queue.poll();
+//            ans.add(temp.val);
+//            if(temp.left == null && temp.right == null){
+//                continue;
+//            }else if(temp.left == null){
+//                queue.add(temp.right);
+//            }else if (temp.right == null){
+//                queue.add(temp.left);
+//            }else {
+//                queue.add(temp.left);
+//                queue.add(temp.right);
+//            }
+//        }
+//        return ans;
+//    }
+
+
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+        while (!queue.isEmpty()){
+            TreeNode temp = queue.poll();
+            if(temp == null){
+                continue;
+            }
+            ans.add(temp.val);
+            queue.add(temp.left);
+            queue.add(temp.right);
         }
         return ans;
     }
