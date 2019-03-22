78dc236a89a5646bdc0311c7202bc0862a30079f
李锴 <1121176110@qq.com>
2019-03-22 21:09:09 +0800

    [add] 补充题目

--- src/leetcode/动态规划/leetcode322.java
+++ src/leetcode/动态规划/leetcode322.java
@@ -1,5 +1,8 @@
 package leetcode.动态规划;
 
+/*
+给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1
+ */
 public class leetcode322 {
 
     public static void main(String[] args){
